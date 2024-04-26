ed72294831a66ec2e0dab4f0d612e7541d93845d
usri.rachid <usri.rachid@gmail.com>
2024-04-26 03:02:30 +0100

    add js countdown

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+let countDownDate = new Date("Sept 01,2024 00:00:00");
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  let delay = countDownDate - now;
 
+  let days = Math.floor(delay / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((delay % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((delay % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let seconds = Math.floor((delay % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
